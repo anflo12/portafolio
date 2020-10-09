ff3de7d6d7ec0d077ee1b564862776137b87a949
andres <andres@andres>
2020-10-08 22:06:13 -0500

    Updates

---
+++ precache-manifest.1e8be3ae27f99cc245d8df5126201fcf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e722180008d4a4e5509b5829b7d5921",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "32836cea57b4d7897fe6",
+    "url": "/portafolio/static/css/main.49e9eea3.chunk.css"
+  },
+  {
+    "revision": "40d5135e2e957ccd2286",
+    "url": "/portafolio/static/js/2.6a52a08a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portafolio/static/js/2.6a52a08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32836cea57b4d7897fe6",
+    "url": "/portafolio/static/js/main.369e3b19.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  },
+  {
+    "revision": "acfaa61aec81ed54beb11b0d2dc8fb20",
+    "url": "/portafolio/static/media/fondoHome.acfaa61a.jpg"
+  },
+  {
+    "revision": "71f62e2d48141e8ff0d72b0c3ca776bb",
+    "url": "/portafolio/static/media/profile.71f62e2d.jpg"
+  }
+]);
