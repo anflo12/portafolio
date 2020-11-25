63a937b657aed1e5ebe7c7511ad5c62609f127f1
andres <amflorez169@misena.edu.co>
2020-11-25 16:24:00 -0500

    ok

---
+++ src/components/LoadingComponents.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loading from './src/assets/Loading.gif'
+export default function LoadingComponents() {
+    return (
+        <div>
+        <img
+           
+            src={Loading}
+            alt="imageProfile"
+          />
+        </div>
+    )
+}
